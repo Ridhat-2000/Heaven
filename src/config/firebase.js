b18eb54454a0e7c8d95db1e69dbd01ac4c4926c2
Ridhat-2000 <ridhat2000@gmail.com>
2025-01-29 16:53:45 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,27 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCj6nvAYL0mdxkjwjYH2--94V9WHY4KnvQ",
-  authDomain: "treasure-hunt-c5053.firebaseapp.com",
-  projectId: "treasure-hunt-c5053",
-  storageBucket: "treasure-hunt-c5053.firebasestorage.app",
-  messagingSenderId: "274175018435",
-  appId: "1:274175018435:web:e6ad1d0882c578ac8c6976"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCj6nvAYL0mdxkjwjYH2--94V9WHY4KnvQ",
+//   authDomain: "treasure-hunt-c5053.firebaseapp.com",
+//   projectId: "treasure-hunt-c5053",
+//   storageBucket: "treasure-hunt-c5053.firebasestorage.app",
+//   messagingSenderId: "274175018435",
+//   appId: "1:274175018435:web:e6ad1d0882c578ac8c6976"
+// };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAgqgfRNeh-IaeQDFYAgpEzhJzbEHJtgP4",
+    authDomain: "jocker-hunt.firebaseapp.com",
+    projectId: "jocker-hunt",
+    storageBucket: "jocker-hunt.firebasestorage.app",
+    messagingSenderId: "121586812453",
+    appId: "1:121586812453:web:963ab07f9421373b30a1cc"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
